8c8760d52ed4380176d6e7b522cdd60fa72f7fab
MayumiNascimento <mayuminascimento9@gmail.com>
2023-12-06 14:44:58 -0300

    start struture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/interfaces/movies.interface.ts
@@ -0,0 +1,11 @@
+export interface IMovies {
+    id: number;
+    title: string;
+    description: string;
+    duration: string;
+    rating: number;
+    gen: string;
+    released: string;
+    trailer: string;
+    img: string;
+}
